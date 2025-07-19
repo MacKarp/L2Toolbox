6df52e15f5019d791f9d6a2189ac76778df74ef1
MacKarp <maciej@karpinski.tech>
2025-07-19 11:16:49 +0200

    Add AppState

---
+++ l2toolbox-gui/src/states/new_profile_setup.rs
@@ -0,0 +1,8 @@
+use crate::app::Message;
+use iced::{Element, widget::Column, widget::Text};
+
+pub fn view() -> Element<'static, Message> {
+    Column::new()
+        .push(Text::new("New Profile Setup Screen"))
+        .into()
+}
